3f16f868e374562abc81a47bc98ede628d57846c
EdGrLab <grabchak1999@gmail.com>
2018-12-19 13:46:41 +0200

    initial commit

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Content extends React.Component {
+  render() {
+     return (
+        <div className="content flx jc-center">
+           <h1>Content</h1>
+        </div>
+     );
+  }
+}
+
+export default Content;
